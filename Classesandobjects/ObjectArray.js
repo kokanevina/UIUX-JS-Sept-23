cde75aad3b05bc73790e0642b261e6cb399d603f
kokaneva <kokane.vina@gmail.com>
2023-09-13 17:25:50 +0530

    commit

--- Classesandobjects/ObjectArray.js
+++ Classesandobjects/ObjectArray.js
@@ -21,6 +21,21 @@ class Student{
 const student1=new Student(100,"Pooja",89.7);
 const student2=new Student(101,"Poonam",90.5);
 const student3=new Student(110,"Raja",89.7);
-const student4=new Student(105,"Pavan",85.5);
+const student4=new Student(105,"Pavan",85);
 const students=[student1,student2,student3,student4, new Student(108,'Kartik',78.9)];
-students.push(new Student(111,'Kiran',88.5));
+students.push(new Student(111,'Kiran',80.5));
+
+// iterate the array using forEach
+students.forEach(student=>console.log(student))
+students.forEach(student=>console.log(student.getDetails()))
+
+// Display students having percentage >= 85  : filter
+let cleverStudents = students.filter(student=>student.studentPercentage>=85)
+cleverStudents.forEach(student=>console.log(student))
+//students.filter(student=>student.studentPercentage>=85).forEach(student=>console.log(student))
+
+// add prefix "Neo-" to every student name and save that student in new array
+students.map(student=>"Neo-"+student.studentName).forEach(ele=>console.log(ele));
+
+students.map(student=>new Student(student.studentId, "Neo-"+student.studentName,student.studentPercentage))
+        .forEach(ele=>console.log(ele));
