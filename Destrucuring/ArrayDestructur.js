a5f630557dc8fbe36bed6cf5306a8e152b5f6e73
kokaneva <kokane.vina@gmail.com>
2023-09-20 17:28:44 +0530

    commit

---
+++ Destrucuring/ArrayDestructur.js
@@ -0,0 +1,17 @@
+
+
+let arr1=[45,67,23,89];
+let arr2=['hello','hi','welcome'];
+
+
+let arr3=[345,"Karuna","LD",'Neosoft',12000.687];  //5
+// store every value of an array in seperate variable  without iteration
+
+const id=arr3[0];
+const ename=arr3[1];
+const dcode=arr3[2];
+/* above method makes the code lengthier : solution : array destructuring*/
+
+const [eid,empname,did,cname,salary]=arr3;
+console.log(eid);
+console.log(cname);
