b6ef5616e1fa37e6b1b7e612c853abdd81b2a58f
kokaneva <kokane.vina@gmail.com>
2023-09-29 17:19:40 +0530

    commit

--- AJAX/ajaxdemo1.js
+++ AJAX/ajaxdemo1.js
@@ -3,15 +3,19 @@
 function makeGetRequest() {
     // First create an XMLHttprequest object
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:3000/books", true);
-    xhr.getResponseHeader("Content-type", "application/json");
-  
-    xhr.onload = function() {
-        console.log(xhr);
-        console.log(xhr.responseText);
-        const books = JSON.parse(this.responseText);
-        console.log(books);
-    }
+    xhr.open("GET", "http://localhost:3000/books");
+    xhr.getResponseHeader("Content-type", "application/json"); //  extra line
     xhr.send();
-    console.log("Hello this is after async req");
+    xhr.onload = function() {
+       // console.log(xhr);   
+       //console.log(xhr.responseText); 
+       //console.log(JSON.parse(xhr.responseText));
+       const books=JSON.parse(xhr.responseText);
+       console.log(books);
+       // display books in table format on html page
+    }
+    xhr.onerror=function(){
+        console.log("something went wrong");
+    }
+
 }
