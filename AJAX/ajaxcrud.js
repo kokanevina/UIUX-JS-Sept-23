c902261a8926bb9fcce78aa3839baa376bf8e680
kokaneva <kokane.vina@gmail.com>
2023-10-04 17:15:48 +0530

    commit

--- AJAX/ajaxcrud.js
+++ AJAX/ajaxcrud.js
@@ -41,4 +41,30 @@ function addBook(){
     // putting all data in book json object
     // making post request
     // collecting server response
+    const id=document.getElementById('id').value;
+    const bookName=document.getElementById('bookName').value;
+    const bookPrice=document.getElementById('bookPrice').value;
+    const bookAuthor=document.getElementById('bookAuthor').value;
+
+    const newBook={
+        "id":parseInt(id),
+        "bookName":bookName,
+        "bookPrice":bookPrice,
+        "bookAuthor":bookAuthor
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", `http://localhost:3000/books`); // data not in address link
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(newBook)); // data sent from request body
+    xhr.onreadystatechange=function(){
+        console.log("SUCCESS");
+        console.log(xhr);
+    }
+    xhr.onprogress=function(){
+        console.log("SUCCESS");
+        console.log(xhr);
+    }
+    xhr.onerror=function(){
+        console.log("something went wrong");
+    }
 }
