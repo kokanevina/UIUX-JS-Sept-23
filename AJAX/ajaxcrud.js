6dd165b2196176346ffa10dc9acfe33e9d7a61c8
kokaneva <kokane.vina@gmail.com>
2023-10-05 17:06:01 +0530

    commit

--- AJAX/ajaxcrud.js
+++ AJAX/ajaxcrud.js
@@ -27,39 +27,43 @@ function searchById(){
     xhr.getResponseHeader("Content-type", "application/json"); //  extra line
     xhr.send();
     xhr.onload = function() {
-     //  const book=JSON.parse(xhr.responseText);
-       console.log(xhr.response);
-       // display books in table format on html page
+       console.log(xhr.responseText);
+       const book=JSON.parse(xhr.responseText);
+       document.getElementById('id').value=book.id;
+       document.getElementById('bookName').value=book.bookName;
+       document.getElementById('bookPrice').value=book.bookPrice;
+       document.getElementById('bookAuthor').value=book.bookAuthor;
     }
     xhr.onerror=function(){
         console.log("something went wrong");
+        
     }
 }
 
+function addorupdate(event){
+    event.preventDefault();
+    console.log(event.submitter);
+    if(event.submitter.id==="addButton")
+        addBook();
+    else
+        updateBook();
+}
+
 function addBook(){
-    // extracting all book details from form
-    // putting all data in book json object
-    // making post request
-    // collecting server response
     const id=document.getElementById('id').value;
     const bookName=document.getElementById('bookName').value;
     const bookPrice=document.getElementById('bookPrice').value;
     const bookAuthor=document.getElementById('bookAuthor').value;
-
     const newBook={
         "id":parseInt(id),  // if u dont set id then json server auto incremnts id
         "bookName":bookName,
-        "bookPrice":bookPrice,
+        "bookPrice":parseFloat(bookPrice),
         "bookAuthor":bookAuthor
     }
     const xhr = new XMLHttpRequest();
     xhr.open("POST", `http://localhost:3000/books`); // data not in address link
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(newBook)); // data sent from request body
-    xhr.onreadystatechange=function(){
-        console.log("SUCCESS");
-        console.log(xhr);
-    }
     xhr.onprogress=function(){
         console.log("SUCCESS");
         console.log(xhr);
@@ -68,6 +72,31 @@ function addBook(){
         console.log("something went wrong");
     }
 }
+
+function updateBook(){
+    const id=document.getElementById('id').value;
+    const bookName=document.getElementById('bookName').value;
+    const bookPrice=document.getElementById('bookPrice').value;
+    const bookAuthor=document.getElementById('bookAuthor').value;
+    const updatedBook={
+        "id":parseInt(id),  // id is existing
+        "bookName":bookName,
+        "bookPrice":parseFloat(bookPrice),
+        "bookAuthor":bookAuthor
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("PUT", `http://localhost:3000/books/${id}`); 
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(updatedBook)); // data sent from request body
+    xhr.onprogress=function(){
+        console.log("SUCCESS");
+        console.log(xhr);
+    }
+    xhr.onerror=function(){
+        console.log("something went wrong");
+    }
+}
+
 
 function deleteById(){
     const bookId=document.getElementById('bid').value;
